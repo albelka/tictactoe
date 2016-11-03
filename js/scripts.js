3647c495ba50c965aef713e15a695f3cce9d6d8b
Levi Bibo <levibibo@gmail.com>
2016-11-02 20:26:02 -0700

    refactor javascript for readibility.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,10 @@
 //Back End
+//Board object
 function Board() {
   this.board = [[0,0,0], [0,0,0], [0,0,0]];
   this.turnsLeft = 9;
   this.gameOver = false;
 };
-
-function Player() {
-  this.board = [[0,0,0], [0,0,0], [0,0,0]];
-  this.winCounter = 0;
-};
-
-Board.prototype.makeMark = function(row, column, player) {
-  this.board[row][column] = player;
-  if(player === "x"){
-    $("#" + row + column).children(".x").show();
-    $("#" + row + column).children(".clear").hide();
-  } else if (player === "o") {
-    $("#" + row + column).children(".o").show();
-    $("#" + row + column).children(".clear").hide();
-  }
-}
 
 //Will count the current turn and end game on a tie
 Board.prototype.countTurns = function(isBlank) {
@@ -33,6 +18,7 @@ Board.prototype.countTurns = function(isBlank) {
   }
 }
 
+//Returns whether the cell at the given coordinates is blank
 Board.prototype.isBlank = function(row, column) {
   if(this.board[row][column]) {
   return false;
@@ -41,9 +27,25 @@ Board.prototype.isBlank = function(row, column) {
   }
 }
 
-Player.prototype.setCell = function(row, column) {
-  this.board[row][column] = 1;
+//Update UI board and game board object at cell at given coordinates with given player
+Board.prototype.makeMark = function(row, column, player) {
+  this.board[row][column] = player.mark;
+  $("#" + row + column).children("." + player.mark).show();
+  $("#" + row + column).children(".clear").hide();
 }
+
+Board.prototype.reset = function() {
+  this.board = [[0,0,0], [0,0,0], [0,0,0]];
+  this.turnsLeft = 9;
+  this.gameOver = false;
+}
+
+
+function Player(mark) {
+  this.board = [[0,0,0], [0,0,0], [0,0,0]];
+  this.winCounter = 0;
+  this.mark = mark;
+};
 
 Player.prototype.checkForWinner = function() {
   var columnTallys = [0,0,0];
@@ -79,60 +81,91 @@ Player.prototype.checkForWinner = function() {
   return isWinner;
 }
 
+Player.prototype.reset = function() {
+  this.board = [[0,0,0], [0,0,0], [0,0,0]];
+}
+
+Player.prototype.setCell = function(row, column) {
+  this.board[row][column] = 1;
+}
+
+Player.prototype.playerWin = function(board) {
+  board.gameOver = true;
+  $("#end").show();
+  $("#winner").text("Player 1 wins!");
+  player1.winCounter++;
+  $("#player1WinCounter").text(player1.winCounter);
+}
+
+
 function parseCoordinates(idString) {
   return [parseInt(idString[0]), parseInt(idString[1])];
 }
 
+function resetObjects(board, player1, player2) {
+  board.reset();
+  player1.reset();
+  player2.reset();
+}
+
+function resetUI() {
+  $(".well").children(".x").hide();
+  $(".well").children(".o").hide();
+  $(".well").children(".clear").show();
+  $("#end").hide();
+}
 
 
+
+
+//Front End
 $(function() {
+  //Initialize objects
   var gameBoard = new Board();
-  var player1 = new Player();
-  var player2 = new Player();
+  var player1 = new Player("x");
+  var player2 = new Player("o");
+
+  //Add click function to each cell
   $("div .well").each(function(cell) {
     $(this).click(function() {
       var coordinates = parseCoordinates($(this).attr("id"));
+
+      //Check to make sure the cell that was clicked is blank and that the game is not over
       if(gameBoard.isBlank(coordinates[0], coordinates[1]) && !gameBoard.gameOver){
+
+        //Update game board and check for a winner (player 1)
         if (gameBoard.turnsLeft % 2 === 1){
           gameBoard.countTurns(gameBoard.isBlank(coordinates[0], coordinates[1]));
-          gameBoard.makeMark(coordinates[0], coordinates[1], "x");
+          gameBoard.makeMark(coordinates[0], coordinates[1], player1);
           player1.setCell(coordinates[0], coordinates[1]);
           if (player1.checkForWinner()){
-            gameBoard.gameOver = true;
-            $("#end").show();
-            $("#winner").text("Player 1 wins!");
-            player1.winCounter++;
-            $("#player1WinCounter").text(player1.winCounter);
+            player1.playerWin(gameBoard);
           };
+
+        //Update game board and check for a winner (player 1)
         } else if (gameBoard.turnsLeft % 2 === 0) {
           gameBoard.countTurns(gameBoard.isBlank(coordinates[0], coordinates[1]));
-          gameBoard.makeMark(coordinates[0], coordinates[1], "o");
+          gameBoard.makeMark(coordinates[0], coordinates[1], player2);
           player2.setCell(coordinates[0], coordinates[1]);
           if (player2.checkForWinner()){
-            gameBoard.gameOver = true;
-            $("#end").show();
-            $("#winner").text("Player 2 wins!");
-            player2.winCounter++;
-            $("#player2WinCounter").text(player2.winCounter);
+            player2.playerWin(gameBoard);
           };
         }
+
+      //Alert players if they try to select a square when the game is over
       } else if (gameBoard.gameOver) {
         alert("The game is over!");
+
+      //Alert player if they try to select a square that has already been selected
       } else {
         alert("You can't do that!");
       }
     })
   })
 
+  //Reset UI and back-end objects for new game
   $("#resetButton").click(function(){
-    $(".well").children(".x").hide();
-    $(".well").children(".o").hide();
-    $(".well").children(".clear").show();
-    $("#end").hide();
-    gameBoard.board = [[0,0,0], [0,0,0], [0,0,0]];
-    player1.board = [[0,0,0], [0,0,0], [0,0,0]];
-    player2.board = [[0,0,0], [0,0,0], [0,0,0]];
-    gameBoard.turnsLeft = 9;
-    gameBoard.gameOver = false;
+    resetObjects(gameBoard, player1, player2);
+    resetUI();
   })
 })
